b802de4738d916e5d446fb16be99e074f3409464
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-04-21 08:21:00 +0300

    bigInt from cf

---
+++ C++/from_cf/main.cpp
@@ -0,0 +1,120 @@
+//#pragma GCC optimize("Ofast,unroll-loops")
+///#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+#define align 64
+
+/*
+
+6 (vs 3)
+for (size_t i = 0; i < NMAX; ++i) {
+            int carry = 0;
+            for (size_t j = 0; j < NMAX || carry != 0; ++j) {
+                long long cur = c[i + j] +
+                    a[i] * 1LL * (j < NMAX ? b[j] : 0) + carry;
+                c[i + j] = static_cast<int>(cur % BASE);
+                carry = static_cast<int>(cur / BASE);
+            }
+        }
+
+
+ for (int i = 0; i < NMAX; ++i) {
+            int carry = 0;
+            for (int j = 0; j < NMAX || carry != 0; ++j) {
+                long long cur = c[i + j] +
+                    a[i] * 1LL * (j < NMAX ? b[j] : 0) + carry;
+                c[i + j] = (cur % BASE);
+                carry = (cur / BASE);
+            }
+        }
+
+
+for (int i = 0; i < NMAX; ++i) {
+            int carry = 0;
+            for (int j = 0; j < NMAX; ++j) {
+                ll& pts = c[i + j];
+                pts += a[i]*b[i];
+                if (pts >= BASE){
+                    pts -= BASE;
+                    ++pts;
+                }
+            }
+        }
+
+3.6 <- good time!
+for (int i = 0; i < NMAX; ++i) {
+            int carry = 0;
+            for (int j = 0; j < NMAX; ++j) {
+                ll& pts = c[i + j];
+                pts += a[i]*b[i];
+                if (pts >= BASE){
+                    pts -= BASE;
+                    ++c[i+j+1];
+                }
+            }
+        }
+
+*/
+
+namespace {
+    template<int n, typename T>
+    void mult(const T *__restrict a, const T *__restrict b, T *__restrict res) {
+        if (n <= 64) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    res[i + j] += a[i] * b[j];
+                }
+            }
+        } else {
+            const int mid = n / 2;
+            alignas(align) T* btmp = new T[n], E[n] = {};
+            auto atmp = btmp + mid;
+            for (int i = 0; i < mid; i++) {
+                atmp[i] = a[i] + a[i + mid];
+                btmp[i] = b[i] + b[i + mid];
+            }
+            mult<mid>(atmp, btmp, E);
+            mult<mid>(a + 0, b + 0, res);
+            mult<mid>(a + mid, b + mid, res + n);
+            for (int i = 0; i < mid; i++) {
+                const auto tmp = res[i + mid];
+                res[i + mid] += E[i] - res[i] - res[i + 2 * mid];
+                res[i + 2 * mid] += E[i + mid] - tmp - res[i + 3 * mid];
+            }
+        }
+    }
+}
+
+using ll = long long;
+
+const int BASE = 10;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    const int NMAX = (1 << 10);
+    alignas(align) static ll a[NMAX], b[NMAX], c[NMAX * 2];
+    for (int i = 0; i < NMAX; i++) a[i] = b[i] = 1;
+
+    for(int k = 0;k<1000;++k){
+
+        for (int i = 0; i < NMAX; ++i) {
+            int carry = 0;
+            for (int j = 0; j < NMAX; ++j) {
+                ll& pts = c[i + j];
+                pts += a[i]*b[i];
+                if (pts >= BASE){
+                    pts -= BASE;
+                    ++c[i+j+1];
+                }
+            }
+        }
+
+        ///mult<NMAX>(a,b,c);
+    }
+    ll sum = 0;
+
+    for (int i = 0; i < 2 * NMAX; i++) sum += c[i];
+    std::cout << sum << std::endl;
+    return 0;
+}
