3b5463dcce1d790d7e8c5f0e248294ce756bcde0
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-10-10 10:06:57 +0300

    aah

--- C++/DortyLibs/DinCBigInt/add.cpp
+++ C++/DortyLibs/DinCBigInt/add.cpp
@@ -1,62 +1,4 @@
 /*
-/// Сложение двух положительных (исключительно контейнеров)
-void BigInt::_add(const BigInt& right) {
-    size_t extra_size = 1;
-
-    size_t an_sz = right.digits.size();
-
-    if (an_sz > digits.size()){
-        extra_size = an_sz - digits.size() + 1;
-    }
-
-    for(size_t i = 0;i < extra_size; i++){
-        digits.push_back(0);
-    }
-    /// Заполняем контейнер нулями, чтобы было место под новые возможные числа(aka разряды)
-
-    for(size_t i = 0; i < an_sz;i++){
-        digits[i] += right.digits[i];
-        if (digits[i] >= BASE){
-            digits[i] -= BASE;
-            digits[i+1]++;
-        }
-    }
-
-    while(digits[an_sz] >= BASE){
-        digits[an_sz] -= BASE;
-        an_sz++;
-        digits[an_sz]++;
-    }
-
-
-
-    _remove_leading_zeros();
-}
-
-/// Обработка вычитания двух положительных чисел (работает, если второе меньше первого)
-void BigInt::_subtract(const BigInt &another){
-
-    size_t an_sz = another.digits.size();
-
-    for(size_t i = 0; i < an_sz;i++){
-        digits[i] -= another.digits[i];
-        if (digits[i] < 0){
-            digits[i] += BASE;
-            digits[i+1]--;
-        }
-    }
-
-    while(digits[an_sz] < 0){
-        digits[an_sz] += BASE;
-        an_sz++;
-        digits[an_sz]--;
-    }
-
-
-
-    _remove_leading_zeros();
-}
-*/
 void BigUnsigned::operator +=(const BigUnsigned& right) {
     if (right.alloc_size > alloc_size){
         /// точно переполнение
@@ -71,29 +13,26 @@ void BigUnsigned::operator +=(const BigUnsigned& right) {
 
 
 }
+*/
 /**
 
-Способно отработать нормально, если ...
+Способно отработать нормально, если a >= minus
 
 + НЕ ЗАБУДЬ ПРО real_size
 
 */
 void BigUnsigned::operator -=(const BigUnsigned& minus) {
 
-    for(int i = 0;i< minus.real_size; ++i){
-        digits[i] -= minus.digits[i];
+    for(size_t i = 0;i< minus.size(); ++i){
+        digits[i] -= minus[i];
         if (digits[i] < 0){
             digits[i] += BASE;
             --digits[i+1];
         }
     }
 
-    if (minus.real_size != real_size){
-
-
-
-        /// Замечу что первый проход не имеет смысла :-)
-        for(int j = minus.real_size; digits[j] < 0;++j){
+    if (minus.size() != size()){
+        for(int j = minus.size(); digits[j] < 0;++j){
 
             digits[j] += BASE;
             --digits[j+1];
@@ -131,15 +70,6 @@ void BigUnsigned::_add(const BigUnsigned& right) {
         ++(*p);
     }
 
-
-    /*
-    CONT_TYPE &p = digits[right.real_size];
-    while (p >= BASE){
-        p -= BASE;
-        (&p)++;
-        ++(p);
-    }
-    */
 
     /// Это тоже следует улучшить
     real_size = std::max(real_size, right.real_size);
