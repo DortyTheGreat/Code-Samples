9fe38d7da26b89f953b0bca58d9851a61befb79b
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-10-20 19:29:44 +0300

    lcm

--- C++/temporary_stuff/Divisions/main.cpp
+++ C++/temporary_stuff/Divisions/main.cpp
@@ -4,31 +4,25 @@
 using namespace std;
 
 
-constexpr uint32_t high_type(uint16_t) {
-    return uint32_t(0);
-}
+constexpr uint32_t __high_type(uint16_t) {return uint32_t(0);}
+constexpr uint64_t __high_type(uint32_t) {return uint64_t(0);}
+constexpr __uint128_t __high_type(uint64_t) {return __uint128_t(0);}
 
-constexpr uint64_t high_type(uint32_t) {
-    return uint64_t(0);
-}
-
-constexpr __uint128_t high_type(uint64_t) {
-    return __uint128_t(0);
-}
+#define high_type(x) decltype(__high_type(x(0)))
 
 template<typename T>
 class fast_div{
 protected:
-    decltype(high_type(T(0))) my_magic = 1;
+    high_type(T) my_magic = 1;
     unsigned char shift;
 
 public:
     fast_div(){}
     fast_div(T val){
-        shift = (sizeof(decltype(high_type(T(0)))) * 8 - 1);
+        shift = (sizeof(high_type(T)) * 8 - 1);
         my_magic = (my_magic << shift) / val;
 
-        while(my_magic >= (decltype(high_type(T(0)))(1) << (sizeof(decltype(high_type(T(0)))) * 4) ) ){
+        while(my_magic >= (high_type(T))(1) << (sizeof(high_type(T)) * 4) ) {
             my_magic >>= 1;--shift;
         }
 
@@ -45,17 +39,20 @@ template<typename T>
 class fast_mod : fast_div<T>{
 private:
     T div;
-
 public:
-
     fast_mod(){}
     fast_mod(T val) : fast_div<T>(val), div(val){}
-
-
     __attribute__((always_inline)) friend T  operator%(const T& val, const fast_mod<T>& fm){
         return val - val/fm * fm.div;
     }
 };
+
+
+#include <vector>
+vector<int> minmax(int a, int b){
+    if (a < b) return {a,b};
+    return {b,a};
+}
 
 template<typename T>
 class LCM{
@@ -75,7 +72,10 @@ public:
 
 int main()
 {
-    cout << 137 % fast_mod(7u);
+    int div;
+    cin >> div;
+    fast_div<uint32_t> fast_div(div); // fast_div<uint32_t, uint64_t> fast_div(div);
+    cout << 137 / div;
 
 
     return 0;
