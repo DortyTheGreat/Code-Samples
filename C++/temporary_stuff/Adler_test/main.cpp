24afb532b6c2f2e3494d4240f1e464b76dc43686
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-11-01 08:30:51 +0300

    wtf?

--- C++/temporary_stuff/Adler_test/main.cpp
+++ C++/temporary_stuff/Adler_test/main.cpp
@@ -169,167 +169,103 @@ constexpr uint8_t type_log_2(uint64_t) {return 6;}
 constexpr uint8_t type_log_2(__uint128_t) {return 7;}
 
 
-template < typename block_type >
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <inttypes.h>
+
 class Sieve{
 
 public:
-    block_type *prime;
+    uint8_t *prime;
 
-    const static uint_fast8_t VEC_BIT =  sizeof(block_type) * 8;
-    const static uint_fast8_t VEC_LOG = type_log_2(block_type(0));
+    constexpr const static uint8_t diff[8] = {6,4,2,4,2,4,6,2};
+    constexpr const static uint8_t bval[8] = {1,7,11,13,17,19,23,29};
 
-    static inline unsigned get(block_type *vec, uintmax_t k) {
-        return (vec[k >> VEC_LOG] >> (k & (VEC_BIT - 1))) & 1;
-    }
-    // Clear bit k in the bit vector vec.
-    static inline void clear(block_type *vec, uintmax_t k) {
-        vec[k >> VEC_LOG] &= ~((block_type)1 << (k & (VEC_BIT - 1)));
-    }
+    constexpr const static uint8_t back_tracker[30] = {0,1 << 0,0,0,0,0,0,1 << 1,0,0,0,1 << 2,0,1 << 3,0,0,0,1 << 4,0,1 << 5,0,0,0,1 << 6,0,0,0,0,0,1 << 7};
+
+    template <typename sz_t>
+    static inline int num2bit(sz_t num) { return (num % 30) * 8 / 30; }
+
+
 
 public:
 
-    Sieve(size_t n){
-        n = n ? (n - 1) >> 1 : 0;   // half of n bits for just the odds
-        n |= VEC_BIT - 1;           // round up to fill the last word
-        size_t len = (n >> 3) + 1;
-
-        prime = (block_type *)malloc(len);
-
-        len >>= VEC_LOG - 3;
-        prime[0] = ~1;                      // 1 is not prime
-        for (unsigned k = 1, p = 3, q = 1; q < VEC_BIT/4;
-                      k++, p += 2, q += k)
-            if (get(prime, k)) {
-                for (unsigned next = q << 2; next < VEC_BIT; next += p){
-                    clear(prime, next);
-                }
-            }
-        if (len == 1)
-            return;
-        // sum the odd primes in the first word, in order to allocate
-        // space for p masks for each odd prime p -- initialize the
-        // masks to all ones
-        unsigned sum = 0;
-        for (unsigned k = 1; k < VEC_BIT; ++k)
-            if (get(prime, k))
-                sum += 2 * k + 1;
-        sum <<= VEC_LOG - 3;        // bytes for s vec_t's
-        block_type *mask = (block_type *)malloc(sum);
-
-        memset(mask, ~0, sum);
-        // build a set of repeating masks for the primes from 3 to
-        // 2*VEC_BIT, for which one or more bits are reset in every
-        // word, where off is the offset of the current p masks in
-        // mask[]
-        for (unsigned k = 1, p = 3, off = 0; k < VEC_BIT;
-                      ++k, p += 2)
-            if (get(prime, k)) {
-                size_t next = (k + VEC_BIT * (p - 1)) % p;
-                size_t end = (p > len - 1 ? len - 1 :
-                                            (size_t)p) << VEC_LOG;
-                for (; next < end; next += p) {
-                    clear(mask + off, next);
-                }
-                off += p;
-            }
-        // now sieve the entire bit vector after the first word using
-        // the repeating masks -- do a chunk at a time to take
-        // advantage of the processor cache (the chunk size was
-        // determined empirically, buying half a second over going
-        // through the entire bit vector for each prime's set of masks)
-
-        size_t start = 1;
-        do {
-            // last is the last vec_t index of this chunk, not past
-            // the end
-            size_t end = start + ((size_t)1 << 12);
-            if (end > len || end < start)
-                end = len;
-            // initialize this chunk to all 1's
-            memset(prime + start, ~0,
-                  (end - start) << (VEC_LOG - 3));
-            // apply masks for each prime to the indices
-            // start..end-1 -- for speed, handle the edge cases
-            // outside of the center loop, which does the vast
-            // majority of the work
-            for (unsigned k = 1, p = 3, off = 0;
-                          k < VEC_BIT; k++, p += 2)
-                if (get(prime, k)) {
-                    size_t next = start;
-                    size_t i = (start - 1) % p;
-                    do {
-                        prime[next++] &= mask[off + i++];
-                    } while (i < p && next < end);
-
-                    while (end - next >= p) {
-                        for (i = 0; i < p;++i ){
-                            prime[next + i] &= mask[off + i];
-                        }
-                        next += p;
-                    }
-
-                    for (i = 0; next < end; ++i, ++next)
-                        prime[next] &= mask[off + i];
-                    off += p;
-                }
-            start = end;
-        } while (start < len);
-
-        // done with masks
-        free(mask);
+    Sieve(size_t max){
+        uint8_t *bmp, *bitmap_end, pattern[3][16*8] = {{0}};
+        uint64_t  a;
+        uint32_t sqrt = sqrtl(max);
 
 
-        uintmax_t incr = ((uintmax_t)1 << 19); /// Это что-то подбирали опытным путём наверное или это L1_CACHE_SIZE
+        uint32_t *as   = (uint32_t*)malloc(sizeof(uint32_t) * ( sqrt / 4 + 1000)),
+                *bmps  = (uint32_t*)malloc(sizeof(uint32_t) * (sqrt / 4 + 1000)),
+                *offs  = (uint32_t*)malloc(sizeof(uint32_t) * 8 * (sqrt / 4 + 1000));
+        int i, j, r, a_i, cnt = 0;
 
-        for(start = 0; start <= n ; start += incr){
-            // last is the last bit location for this chunk, not
-            // past the end
-            uintmax_t last = start + (incr - 1);
-            if (last > n) last = n;
-            // clear composites at bit locations start..last
 
-            for (uintmax_t k = VEC_BIT;;++k) {
-                if (get(prime, k)) {
-                    // start at the location of p squared (before
-                    // there, primes less than p already cleared
-                    // multiples of p)
-                    uintmax_t next = 2 * k * (k + 1);
-                    // p is the prime that corresponds to bit
-                    // location k
-                    uintmax_t p = 2 * k + 1;
-                    // if the square of p is past the end of this
-                    // chunk, then all of the subsequent primes
-                    // will be too
-                    if (next > last)
-                        break;
-                    // add just enough p's to the bit location to
-                    // put it in this chunk (this effectively adds
-                    // just enough 2*p's to the corresponding odd
-                    // integer)
-                    if (next < start) {
-                        next = k + p - start % p;
-                        if (next >= p) next -= p;
-                        next += start;
-                    }
-                    // clear composites, effectively adding 2*p to
-                    // the current odd integer in order to get to
-                    // the next odd integer
-                    for (; next <= last; next += p) {
-                        clear(prime, next);
-                    }
-                }
+        /* Init lower primes */
+        for (i = 1; i < 4; ++i)
+          for (r = bval[i], a_i = 0; r < bval[i] * 30 * 8; r += bval[i] * diff[a_i++ % 8])
+             pattern[i-1][r / 30] |= 1 << num2bit(r);
 
-            }
+        /*
+        * Find sieving primes
+        */
+        bitmap_end = prime + sqrt + 1;
 
+        for (i = 1; i < 4; i++)
+          for (bmp = prime; bmp < bitmap_end; bmp += bval[i]*8)
+             for (j = 0; j < bval[i]; j++)
+                *((uint64_t *)bmp + j) |= *((uint64_t *)pattern[i-1] + j);
+
+        for (a = 17, a_i = 4; a <= sqrt; a += diff[a_i++%8]) {
+          if (prime[a / 30] & 1 << (a_i%8))
+             continue;
+
+          as[cnt] = a;
+          for (i = 0; i < 8; i++)
+             offs[cnt*8 + num2bit(a * bval[i])] = a * bval[i] / 30;
+
+          for (bmp = prime + (uint64_t)a * (a / 30); bmp < bitmap_end; bmp += a)
+             for (i = 0; i < 8; i++)
+                *(bmp + offs[cnt*8 + i]) |= 1 << i;
+
+          bmps[cnt++] = (bmp - prime);
         }
+
+        /*
+        * Calculate blocks
+        */
+        bitmap_end = prime + 32*1024;
+        for (uint64_t cur = 0; cur <= max; cur += 32*1024*30, bitmap_end += 32*1024) {
+
+          for (i = 1; i < 4; i++)
+             for (bmp = prime + cur/30/(8*bval[i])*(bval[i]*8); bmp < bitmap_end; bmp += bval[i]*8)
+                for (j = 0; j < bval[i]; j++)
+                   *((uint64_t *)bmp + j) |= *((uint64_t *)pattern[i-1] + j);
+
+          for (i = 0; i < cnt; i++)
+             for (; bmps[i] < cur/30 + 32*1024; bmps[i] += as[i])
+                for (j = 0; j < 8; j++)
+                   *(prime + bmps[i] + offs[i*8 + j]) |= 1 << j;
+        }
+
+
+        /* First byte gets clobbered for the first block */
+        prime[0] = 0x1;
+
+        free(as);
+        free(bmps);
+        free(offs);
 
     }
 
     bool isPrime(int num){
-        if (num < 3) return num == 2;
-        if (num % 2 == 0) return false;
-        num /= 2;
-        return prime[num / 64] & (1ull << (num % 64));
+        if (num % 2 == 0) return num == 2;
+        if (num % 3 == 0) return num == 5;
+        if (num % 5 == 0) return num == 5;
+
+        return prime[num / 30] & back_tracker[num % 30];
     }
 
     ~Sieve(){
@@ -360,7 +296,7 @@ int main(void) {
 
 
 
-    Sieve<uint64_t> S(1001 * 1000 * 1000);
+    Sieve S(1001 * 1000 * 1000);
 
     int a,ol, b,c, n;
     cin >> a >> ol >> b >> c >> n;
---
+++ C++/temporary_stuff/rus_lang/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /// Здарова всем, ЙО!
+    cout << "Hello world!" << endl;
+
+
+    return 0;
+}
