0f84b089322bf2d000f5e9284e506664f69ed0c0
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-11-03 10:02:50 +0300

    stack

--- C++/temporary_stuff/solution2/main.cpp
+++ C++/temporary_stuff/solution2/main.cpp
@@ -13,19 +13,38 @@ int c2;
 
 int t = 666;
 int s;
+
+void print(int path){
+    bool flag = 0;
+    for(int i = 30; i > -1; --i){
+        bool v = ((path >> i) & 1);
+        if (flag) cout << "fs"[((path >> i) & 1)];
+        if (v) flag = 1;
+    }
+    exit(0);
+}
+
 void reqursion(uint64_t num, int path){
     if (path > (1 << (K+1)) ) return;
-    if (num == t) {
-        bool flag = 0;
-        for(int i = 30; i > -1; --i){
-            bool v = ((path >> i) & 1);
-            if (flag) cout << "fs"[((path >> i) & 1)];
-            if (v) flag = 1;
-        }
-        exit(0);
-    }
+    if (num == t) print(path);
     reqursion((num * a1 + b1) % c1, path*2);
     reqursion((num * a2 + b2) % c2, path*2+1);
+}
+
+#include <stack>
+#include <deque>
+void requir_opt(uint64_t num){
+
+    deque<uint64_t> deque1;
+    stacke.push(num);
+
+    int path;
+    for(path = 1;num != t; ++path){
+        stacke.push((num * a1 + b1) % c1);
+        stacke.push((num * a2 + b2) % c2);
+        num = stacke.top();
+    }
+    print(path);
 }
 
 void solve() {
