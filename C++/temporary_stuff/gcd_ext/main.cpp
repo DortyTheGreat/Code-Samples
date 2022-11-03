c8dafd33a33b27f3696fc0b2a6f2e49333871161
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-11-03 12:14:39 +0300

    nice

---
+++ C++/temporary_stuff/gcd_ext/main.cpp
@@ -0,0 +1,115 @@
+
+
+
+#include <iostream>
+#include <utility>
+using namespace std;
+/// source : https://www.cyberforum.ru/cpp-beginners/thread2679363.html
+
+/*
+
+template <typename T>
+T gcd_ext(T a,T b,T& x,T& y) {
+    if (b==0){
+        x=1;
+        y=0;
+        return a;
+    }
+    T d=gcd_ext(b,a%b,y,x);
+    y-=(a/b)*x;
+    return d;
+}
+*/
+
+template <typename T>
+T gcd_ext(T a,T b,T& x,T& y) {
+
+    while(b){
+
+    }
+    x = 1;
+    y = 0;
+    if (b==0){
+        x=1;
+        y=0;
+        return a;
+    }
+    T d=gcd_ext(b,a%b,y,x);
+    y-=(a/b)*x;
+    return d;
+}
+
+/// https://stackoverflow.com/questions/22991075/extended-euclid-algorithm-is-there-non-recursive-version-of-it
+template <typename T>
+T extended_gcd(T a,T b, T& x, T& y){
+    T unPrev = 1;
+    T vnPrev = 0;
+    x = 0;
+    y = 1;
+    T bn;
+    while (b){
+        bn = a / b;
+        unPrev -= bn * x;
+        vnPrev -= bn * y;
+        /// Теперь prev - это новый cur
+        if (! (a%=b) ) return b;
+
+        bn = b / a;
+        b %= a;
+        x -= bn * unPrev;
+        y -= bn * vnPrev;
+
+
+    }
+    x = unPrev;
+    y = vnPrev;
+    return a;
+}
+/// a*x + b*y = c
+/// даёт (-1,-1), если решения нет, в ином случае решение с минимальным неотрицательным x.
+pair<int,int> diophantus(int a, int b, int c){
+    int x0,y0;
+
+    int d=extended_gcd(a,b,x0,y0);
+    if(c%d){
+        return {-1,-1};
+    }
+    int p=x0*c/d;
+    int q=b/d;
+    int x=(p%q+q)%q;
+    int y=(c-a*x)/b;
+    return {x,y};
+}
+bool f3(int n, int a, int b){
+    return (diophantus(a,b,n).second >=0);
+}
+
+bool f_(int n, int a, int b) {
+    for (int k = 0; k * a <= n; ++k) {
+        if ((n - k * a) % b == 0)
+            return true;
+    }
+    return false;
+}
+
+int main()
+{
+    cout << f3(15,5,7) << endl;
+
+    for(int n = 1;n < 300;++n){
+        for(int x = 1;x < 300; ++x){
+            for (int y = 1; y < 300; ++y){
+                if (f_(n,x,y) != f3(n,x,y)){
+                    cout << n << " " << x << " "  << y << endl;
+                    cout << "gosunov: " << f_(n,x,y) << endl;
+                    cout << "dorty4: " << f3(n,x,y) << endl << endl;
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
