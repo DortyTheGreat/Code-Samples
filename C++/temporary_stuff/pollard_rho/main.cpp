c3b92588e0fb02417aa01ffe842daa70ba82ef4a
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2022-12-02 19:59:21 +0300

    компьютерная графика - отстой.
    особенно компас., особенно v12, тем более лимитированная школьная версия. Буэ

--- C++/temporary_stuff/pollard_rho/main.cpp
+++ C++/temporary_stuff/pollard_rho/main.cpp
@@ -28,49 +28,136 @@ This trick made the implementation 4 times faster.
 #include <cstdint>
 #include <iostream>
 #include <tuple>
+#include <vector>
 
 
+#define FORCE_INLINE __attribute__((always_inline)) inline
+
+uint64_t ret, low;
+/// Возвращает большую часть от 128: 64x64
+FORCE_INLINE uint64_t hmul64(const uint64_t& a,const uint64_t& b){
+    asm ("mulq %3" : "=a"(low), "=d"(ret) : "a"(a), "g"(b) : "cc");
+    return ret;
+}
+
+FORCE_INLINE auto ctz(const uint64_t& num){
+    return __builtin_ctzll(num);
+}
+
+FORCE_INLINE auto ctz(const uint32_t& num){
+    return __builtin_ctz(num);
+}
+
+template<typename unsign>
+FORCE_INLINE unsign sqr(const unsign& num){
+    return num*num;
+}
+
+class fast_div64{
+protected:
+    uint64_t my_magic = 1;
+    uint_fast8_t shift;
+
+public:
+    fast_div64(){}
+    fast_div64(const uint64_t& val){
+        shift = (127);
+
+        __uint128_t temp = (__uint128_t(1) << 127) / val;
+        shift = __builtin_clzll(temp  >> 64); /// zeros
+        my_magic = (temp >> (64-shift));
+        --shift;
+
+        if (!((++my_magic) & 1)){my_magic >>= 1; --shift;}
+        ///cout << my_magic << " " << (int)shift << endl;
+    }
+
+
+    FORCE_INLINE friend uint64_t operator/(const uint64_t& val, const fast_div64& fd){
+
+
+
+        return (hmul64(val,fd.my_magic) >> fd.shift ) ;
+    }
+};
+
+
+class fast_mod64 : fast_div64{
+private:
+    uint64_t div;
+public:
+    fast_mod64(){}
+    fast_mod64(uint64_t val) : fast_div64(val), div(val){}
+    FORCE_INLINE friend uint64_t operator%(const uint64_t& val, const fast_mod64& fm){
+        return val - val/fm * fm.div;
+    }
+};
+
+
+template<typename T, typename quick_mod>
+class LCM{
+public:
+    T mult, old_rand, add;
+    quick_mod fm;
+    LCM(T _mult,T _old_rand,T _add,T _mod) : mult(_mult), old_rand(_old_rand), add(_add), fm(_mod){}
+    FORCE_INLINE T get(){
+        return old_rand = (mult * old_rand + add) % fm;
+    }
+
+};
 
 
 // Computes (a + b) % m, assumes a < m and b < m.
 template<typename T>
-T addmod(T a, T b, T m) {
+FORCE_INLINE T addmod(const T& a,const T& b,const T& m) {
     if (b >= m - a) return a - m + b;
     return a + b;
 }
 
-template<typename T>
-static T mult(T x, T y,T& HighProduct) {
-    const size_t shift = sizeof(x) * 4;
-    T a = x >> shift, b = (x << shift) >> shift;
-    T c = y >> shift, d = (y << shift) >> shift;
-    // (a*2^64 + b) * (c*2^64 + d) =
-    // (a*c) * 2^128 + (a*d + b*c)*2^64 + (b*d)
-    T ac = a * c;
-    T ad = a * d;
-    T bc = b * c;
-    T bd = b * d;
-    T carry = ((ad << shift) >> shift) + ((bc << shift) >> shift) + (bd >> shift);
-    HighProduct = ac + (ad >> shift) + (bc >> shift) + (carry >> shift);
-    return (ad << shift) + (bc << shift) + bd;
-}
+
 
 // Computes aR * bR mod N with R = 2**64.
 
 
-uint64_t montmul(__uint128_t a, uint64_t b, __uint128_t mod, uint64_t inv){
-    a *= b;
-    uint64_t y = (a >> 64) - (__uint128_t(uint64_t(a) * inv) * mod >> 64);
-    return int64_t(y) < 0 ? y + mod : y;
+FORCE_INLINE uint64_t montmul(const uint64_t& a,const uint64_t& b,const uint64_t& mod,const uint64_t& inv){
+    //a *= b;
+    hmul64(a,b);
+    uint64_t high = ret;
+
+    hmul64((low * inv), mod); /// get high
+
+    int64_t y = high - ret;
+    return (y) < 0 ? y + mod : y;
 
 }
 
-/// Чуть быстрее __gcd, для int
-template<typename T>
-inline T gcd(T a, T b)
-{
-    while(a %=b) if (!(b %= a)) return a;
-    return b;
+/**
+ * \brief Greatest common divisor.
+ * \ingroup num
+ *
+ * `std::gcd` is available since C++17, but the GCC implementaiton uses the slow Euclidean algorithm until version 11.
+ * This implementation uses binary GCD which is generally several times faster.
+ *
+ * Note that, unlike `std::gcd`, this function only accepts unsigned integers.
+ */
+template<typename T, std::enable_if_t<std::is_unsigned_v<T>>* = nullptr>
+constexpr T gcd(T x, T y) {
+    if (x == 0) {
+        return y;
+    } else if (y == 0) {
+        return x;
+    }
+    int kx = ctz(x);
+    int ky = ctz(y);
+    x >>= kx;
+    while (y != 0) {
+        y >>= ctz(y);
+        if (x > y) {
+            std::swap(x, y);
+        }
+        y -= x;
+    }
+    return x << std::min(kx, ky);
 }
 
 
@@ -214,7 +301,7 @@ __uint128_t mod_pow(__uint128_t a, __uint128_t t, __uint128_t mod) {
 bool isPrime(unsigned long long n) {
     /// Китаец сказал, что 2, 3, 5, 7, 11, 13, 17, 19 для ull
     /// 2, 3, 5 - наверное следует использовать для интов, но 2 и 3 заходит для https://vjudge.net/problem/SPOJ-PRIME1
-    static const int jp[] = { 2,3, 5,7,11,13,17};
+    static const int jp[] = { 2,3, 5,7,11};
 
     if (n == 1)
         return false;
@@ -256,6 +343,72 @@ bool isPrime(unsigned long long n) {
     return true;
 }
 
+uint16_t bases[]={15591,2018,166,7429,8064,16045,10503,4399,1949,1295,2776,3620,560,3128,5212,
+2657,2300,2021,4652,1471,9336,4018,2398,20462,10277,8028,2213,6219,620,3763,4852,5012,3185,
+1333,6227,5298,1074,2391,5113,7061,803,1269,3875,422,751,580,4729,10239,746,2951,556,2206,
+3778,481,1522,3476,481,2487,3266,5633,488,3373,6441,3344,17,15105,1490,4154,2036,1882,1813,
+467,3307,14042,6371,658,1005,903,737,1887,7447,1888,2848,1784,7559,3400,951,13969,4304,177,41,
+19875,3110,13221,8726,571,7043,6943,1199,352,6435,165,1169,3315,978,233,3003,2562,2994,10587,
+10030,2377,1902,5354,4447,1555,263,27027,2283,305,669,1912,601,6186,429,1930,14873,1784,1661,
+524,3577,236,2360,6146,2850,55637,1753,4178,8466,222,2579,2743,2031,2226,2276,374,2132,813,
+23788,1610,4422,5159,1725,3597,3366,14336,579,165,1375,10018,12616,9816,1371,536,1867,10864,
+857,2206,5788,434,8085,17618,727,3639,1595,4944,2129,2029,8195,8344,6232,9183,8126,1870,3296,
+7455,8947,25017,541,19115,368,566,5674,411,522,1027,8215,2050,6544,10049,614,774,2333,3007,
+35201,4706,1152,1785,1028,1540,3743,493,4474,2521,26845,8354,864,18915,5465,2447,42,4511,
+1660,166,1249,6259,2553,304,272,7286,73,6554,899,2816,5197,13330,7054,2818,3199,811,922,350,
+7514,4452,3449,2663,4708,418,1621,1171,3471,88,11345,412,1559,194};
+
+
+
+/// важно, чтобы тип unsign позволял вместить в себя произведение mod*mod. aka избавляйтесь от переполнения
+template<typename unsign, typename power_type, typename modulus_obj>
+FORCE_INLINE unsign modul_pow(unsign num, power_type power, const modulus_obj& mod){
+    unsign cur = 1;
+    for(;power;power >>= 1) {
+        if (power & 1) cur = (cur*num) % mod;
+        num = sqr(num) % mod;
+    }
+    return cur;
+}
+
+/// http://ceur-ws.org/Vol-1326/020-Forisek.pdf
+bool is_SPRP(uint32_t n, const uint64_t& a) {
+
+    fast_mod64 fm_n(n);
+    uint_fast8_t s = ctz(--n);
+    uint64_t cur = modul_pow(a, n>>s, fm_n);
+    if (cur == 1) return true;
+    for (uint_fast8_t r=0; r<s; ++r) {
+        if (cur == n) return true;
+        cur = sqr(cur) % fm_n;
+    }
+    return false;
+}
+
+/// check factor
+/// check factor
+#define cf(t) if (!(x%t)){ return (x==t);}
+
+
+#define _5c(a,b,c,d,e) cf(a) cf(b) cf(c) cf(d) cf(e)
+
+bool is_prime_WTF(uint32_t x) {
+    /*
+    _5c(2,3,5,7,11)
+    _5c(13,17,19,23,29)
+    _5c(31,37,41,43,47)
+    _5c(53,59,61,67,71)
+    _5c(73,79,83,89,97)
+    */
+    if (x<121) return (x>1);
+    uint64_t h = x;
+    h = ((h >> 16) ^ h) * 0x45d9f3b;
+    h = ((h >> 16) ^ h) * 0x45d9f3b;
+    h = ((h >> 16) ^ h) & 255;
+    return is_SPRP(x,bases[h]);
+}
+
+
 #include <algorithm>
 ///  const __uint128_t& ui128, но у меня всё деструктивно
 std::ostream& operator <<(std::ostream& os, __uint128_t ui128) {
@@ -273,36 +426,36 @@ std::ostream& operator <<(std::ostream& os, __uint128_t ui128) {
 	return (os << buff);
 }
 
-#define cf(k) while (!(num % k)){arr[size++] = k; num /= k;}
+#define cf3(k) while (!(num % k)){arr[size++] = k; num /= k;}
 
-std::vector<uint64_t> factor(__uint128_t num){
-
+std::vector<uint64_t> factor(uint64_t num){
+    ///cout <<"F: " << num << endl;
     vector<uint64_t> arr(sizeof(uint64_t) * 8 - 1);
     unsigned int size = 0;
 
-    cf(2) cf(3) cf(5) cf(7) cf(11)
-
+    cf3(2) cf3(3) cf3(5) cf3(7) cf3(11)
+    cf3(13) cf3(17) cf3(19) cf3(23) cf3(29)
+    cf3(31) cf3(37) cf3(41) cf3(43) cf3(47)
+    if (num == 1) goto last;
     arr[size] = num;
 
 
 
     for(size_t i = size;i < size + 1; ){
         ///cout << arr[i] << endl;
+        ///cout << isPrime(arr[i]) << endl;
         if(arr[i] == 1){++i; continue;}
-        if (isPrime(arr[i])) { ++i; continue; } /// got here n as a factor
+        if (is_prime_WTF(arr[i])) { ++i; continue; } /// got here n as a factor
         arr[++size] = pollard_brent_montgomery( arr[i]);
 
         arr[i] /= arr[size];
 
     }
-
-    arr.resize(size);
-    sort(arr.begin(),arr.end());
-
-
-
-
-    return arr;
+    ++size;
+    last:
+        arr.resize(size);
+        sort(arr.begin(),arr.end());
+        return arr;
 }
 
 
@@ -324,67 +477,7 @@ std::istream& operator>>(std::istream& in, __uint128_t &ui128) {
 }
 
 
-#define FORCE_INLINE __attribute__((always_inline)) inline
 
-uint64_t ret, low;
-/// Возвращает большую часть от 128: 64x64
-FORCE_INLINE uint64_t hmul64(const uint64_t& a,const uint64_t& b){
-    asm ("mulq %3" : "=a"(low), "=d"(ret) : "a"(a), "g"(b) : "cc");
-    return ret;
-}
-
-class fast_div64{
-protected:
-    uint64_t my_magic = 1;
-    uint_fast8_t shift;
-
-public:
-    fast_div64(){}
-    fast_div64(const uint64_t& val){
-        shift = (127);
-
-        __uint128_t temp = (__uint128_t(1) << 127) / val;
-        shift = __builtin_clzll(temp  >> 64); /// zeros
-        my_magic = (temp >> (64-shift));
-        --shift;
-
-        if (!((++my_magic) & 1)){my_magic >>= 1; --shift;}
-        ///cout << my_magic << " " << (int)shift << endl;
-    }
-
-
-    FORCE_INLINE friend uint64_t operator/(const uint64_t& val, const fast_div64& fd){
-
-
-
-        return (hmul64(val,fd.my_magic) >> fd.shift ) ;
-    }
-};
-
-
-class fast_mod64 : fast_div64{
-private:
-    uint64_t div;
-public:
-    fast_mod64(){}
-    fast_mod64(uint64_t val) : fast_div64(val), div(val){}
-    FORCE_INLINE friend uint64_t operator%(const uint64_t& val, const fast_mod64& fm){
-        return val - val/fm * fm.div;
-    }
-};
-
-
-template<typename T, typename quick_mod>
-class LCM{
-public:
-    T mult, old_rand, add;
-    quick_mod fm;
-    LCM(T _mult,T _old_rand,T _add,T _mod) : mult(_mult), old_rand(_old_rand), add(_add), fm(_mod){}
-    FORCE_INLINE T get(){
-        return old_rand = (mult * old_rand + add) % fm;
-    }
-
-};
 
 template <typename T>
 std::ostream& operator<<(std::ostream &in, const std::vector<T> &vect) {
@@ -395,17 +488,32 @@ std::ostream& operator<<(std::ostream &in, const std::vector<T> &vect) {
     return in;
 }
 
+template <typename T>
+uint64_t summ(const std::vector<T> &vect) {
+    int n = vect.size();
+    uint64_t s = 0;
+    for(int i =0;i<n;i++){
+        s += vect[i];
+    }
+    return s;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
+    int a,ol, b,c, n;
+    cin >> a >> ol >> b >> c >> n;
+    LCM<uint64_t, fast_mod64> myl(a,ol,b,c);
+    uint64_t ans = 0;
 
     for(int i = 0;i< n;++i){
-        uint64_t t;
-        cin >> t;
-        cout << factor(t);
+        ///auto v= myl.get();
+        ///cout << v << endl;
+
+        ans += summ(factor(myl.get()));
+        ///cout << endl;
 
     }
+    cout << ans;
     return 0;
 
 
