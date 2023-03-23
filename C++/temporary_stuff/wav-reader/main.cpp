9cc108dee98c1a3389ad01042c362b40191ca81e
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2023-03-23 15:45:22 +0300

    bit_swap + ear_swap

--- C++/temporary_stuff/wav-reader/main.cpp
+++ C++/temporary_stuff/wav-reader/main.cpp
@@ -6,6 +6,10 @@ double M_PI = acos(-1);
 
 using namespace std;
 
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
 int main()
 {
     std::cout << "**********************" << std::endl;
@@ -15,31 +19,50 @@ int main()
     //---------------------------------------------------------------
     // 1. Let's setup our AudioFile instance
 
-    AudioFile<float> a;
-    a.setNumChannels (2);
-    a.setNumSamplesPerChannel (44100 * 100);
 
-    //---------------------------------------------------------------
-    // 2. Create some variables to help us generate a sine wave
+    AudioFile<float> aria;
 
-    const float sampleRate = 44100.f;
-    const float frequencyInHz = 340.f;
+    AudioFile<float> new_aria;
 
-    //---------------------------------------------------------------
-    // 3. Write the samples to the AudioFile sample buffer
+    bool loadedOK = aria.load ("money.wav");
 
-    for (int i = 0; i < a.getNumSamplesPerChannel(); i++)
+    new_aria = aria;
+    float gain = 2.5f;
+
+    float max_aria = -100000;
+
+    int64_t Hz = 44100.0 / ( 115.0 / 60.0);
+
+
+
+    float p = 0.1;
+    float q = 0.1;
+
+    for (int64_t i = 0; i < aria.getNumSamplesPerChannel(); i++)
     {
-        for (int channel = 0; channel < a.getNumChannels(); channel++)
-        {
-            a.samples[channel][i] = (i*i)/(10000.0); ///sin ((static_cast<float> (i * i / 10 / 1000) / sampleRate) * frequencyInHz * 2.f * M_PI);
+        int offset = 2 * (!( (i/Hz) %2)) - 1;
+
+        int64_t real_i = i + offset*Hz;
+        if (real_i < 0 || real_i >= aria.getNumSamplesPerChannel() ){
+            real_i = i;
         }
+        if (offset == 1){
+            new_aria.samples[0][i] = aria.samples[0][real_i];
+            new_aria.samples[1][i] = aria.samples[1][real_i];
+        }else{
+            new_aria.samples[1][i] = aria.samples[0][real_i];
+            new_aria.samples[0][i] = aria.samples[1][real_i];
+        }
+
+            ///gangsta.samples[channel][i] = 2 * sgn(gangsta.samples[channel][i]) / ( 1/abs(gangsta.samples[channel][i]) + 1/abs(aria.samples[channel][i]) ) ;
+            ///gangsta.samples[channel][i] = (gangsta.samples[channel][i] * max( (abs(aria.samples[channel][i]) / max_aria ), 0.2f) );
+
     }
 
     //---------------------------------------------------------------
-    // 4. Save the AudioFile
+    // 4. Write audio file to disk
 
-    std::string filePath = "sine-wave.wav"; // change this to somewhere useful for you
-    a.save ("sine-wave.wav", AudioFileFormat::Wave);
+    std::string outputFilePath = "quieter-audio-filer.wav"; // change this to somewhere useful for you
+    new_aria.save (outputFilePath, AudioFileFormat::Wave);
     return 0;
 }
