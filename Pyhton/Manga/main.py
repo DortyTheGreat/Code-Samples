b963639795d0c1953a4cd98eea9c453fa1e52186
Alexan <72660192+DortyTheGreat@users.noreply.github.com>
2023-04-05 11:35:44 +0300

    revert this

--- Pyhton/Manga/main.py
+++ Pyhton/Manga/main.py
@@ -13,21 +13,49 @@ for image in res:
     image_path = 'content\\hero\\18\\' + image
     img = cv2.imread(image_path)
 
-    d = pytesseract.image_to_data(img, output_type=Output.DICT, lang="eng", config="hocr")
-    print(d)
-    n_boxes = len(d['level'])
-    for i in range(n_boxes):
-        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, d['level'][i] * 50, 0), 2)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+
+
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+    im2 = img.copy()
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
 
 
 
-    #h, w, c = img.shape
+        # Drawing a rectangle on copied image
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Cropping the text block for giving input to OCR
+        cropped = im2[y:y + h, x:x + w]
 
-    #boxes = pytesseract.image_to_boxes(img)
-    #pytesseract.image_to_boxes(image, lang="eng", output_type=)
+        # Open the file in append mode
+        file = open("recognized.txt", "a")
+
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(cropped)
+
+        file.write(text)
+        file.write("\n")
+
+        # Appending the text into file
+        print(text)
 
 
 
-    cv2.imwrite('content\\hero\\18\\out\\' + image, img)
+
+
+
+
+
+    cv2.imwrite('content\\hero\\18\\out\\' + image, im2)
     #cv2.waitKey(0)
